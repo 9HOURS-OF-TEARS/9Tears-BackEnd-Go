62694d746454392bf7cd216cf1091282f1b5490c
jangwoo kim <kjw2262@gmail.com>
2021-11-25 17:47:17 +0900

    [FEATURE] 이모지 제거(댓글)

---
+++ controller/comment/emoji/delete.go
@@ -0,0 +1,33 @@
+package emoji
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"Hackathon/model"
+	dto "Hackathon/model/DTO"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func Delete() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ID, err := strconv.Atoi(c.Param("comment_id"))
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		if err := model.DB.Delete(&dto.CommentEmoji{CommentID: uint(ID), UserID: c.GetString("user_id")}).Error; err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				c.Status(http.StatusBadRequest)
+				return
+			} else {
+				panic(err)
+			}
+		}
+
+		c.Status(http.StatusOK)
+	}
+}
