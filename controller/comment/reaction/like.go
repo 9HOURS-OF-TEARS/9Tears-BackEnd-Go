b60583c1c88ff49069ae394affba490ddb5324d5
jangwoo kim <kjw2262@gmail.com>
2021-11-25 17:44:48 +0900

    [FEATURE] 좋아요 기능(댓글)

---
+++ controller/comment/reaction/like.go
@@ -0,0 +1,41 @@
+package reaction
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"Hackathon/model"
+	dto "Hackathon/model/DTO"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func Like() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ID, err := strconv.Atoi(c.Param("comment_id"))
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		if err := model.DB.Exec("update comment set like_count = like_count + 1 where id = ?", ID).Error; err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				c.Status(http.StatusBadRequest)
+				return
+			} else {
+				panic(err)
+			}
+		}
+
+		if err := model.DB.Create(&dto.CommentLike{
+			UserID:    c.GetString("user_id"),
+			CommentID: uint(ID),
+			IsLike:    true,
+		}); err != nil {
+			panic(err)
+		}
+
+		c.Status(http.StatusCreated)
+	}
+}
