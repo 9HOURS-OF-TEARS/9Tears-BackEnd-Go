adb9fd9378ff98160006bb17124bb7d13296edc2
jangwoo kim <kjw2262@gmail.com>
2021-11-25 17:46:35 +0900

    [FEATURE] 칭찬 스티커 제거

---
+++ controller/post/sticker/delete.go
@@ -0,0 +1,33 @@
+package sticker
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"Hackathon/model"
+	dto "Hackathon/model/DTO"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func Delete() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ID, err := strconv.Atoi(c.Param("post_id"))
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		if err := model.DB.Delete(&dto.PostSticker{PostID: uint(ID)}).Error; err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				c.Status(http.StatusBadRequest)
+				return
+			} else {
+				panic(err)
+			}
+		}
+
+		c.Status(http.StatusOK)
+	}
+}
