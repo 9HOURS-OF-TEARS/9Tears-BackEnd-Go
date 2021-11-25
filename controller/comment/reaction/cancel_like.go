82ee3383fb490fe813a77570c75de71d8d0580ea
jangwoo kim <kjw2262@gmail.com>
2021-11-25 17:49:23 +0900

    [FEATURE] 좋아요 취소(댓글)

---
+++ controller/comment/reaction/cancel_like.go
@@ -0,0 +1,42 @@
+package reaction
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"Hackathon/model"
+	dto "Hackathon/model/DTO"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func CancelLike() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ID, err := strconv.Atoi(c.Param("comment_id"))
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		if err := model.DB.Exec("update comment set like_count = like_count - 1 where id = ?", ID).Error; err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				c.Status(http.StatusBadRequest)
+				return
+			} else {
+				panic(err)
+			}
+		}
+
+		if err := model.DB.Delete(&dto.CommentLike{CommentID: uint(ID), UserID: c.GetString("user_id")}).Error; err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				c.Status(http.StatusBadRequest)
+				return
+			} else {
+				panic(err)
+			}
+		}
+
+		c.Status(http.StatusOK)
+	}
+}
